411ec51a1cb78d574100bd86d1bec5f4e5072ccc
Argiris888 <59976040+Argiris888@users.noreply.github.com>
2020-05-13 03:02:22 +0300

    may

--- pame.py
+++ pame.py
@@ -1,1 +1,21 @@
-print('vs')
+mylist=list((1,2,6,3,7,2))
+print(mylist)
+#mylist[:]=[]
+#del mylist[:]
+#mylist.extend([2])
+#item_of_list=mylist.pop(2)
+#print(mylist,item_of_list)
+###mylist.sort(reverse=False) #ginetai monima to sortarisma!
+##mylist.append(53) #to idio
+##mylist.reverse()
+##mylist.clear()
+##mylist.insert(2,86)
+#print(7 in mylist)  
+#print(mylist)
+a='ela edw re'
+#print(a.replace('d','e'))
+#print(a)
+
+
+
+
